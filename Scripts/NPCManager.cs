ab67fd809c067c80b7f199037f86bb259e69ee9c
ToddSandberg <sandbergs.todd@gmail.com>
2024-10-12 11:14:43 -0700

    Create family function

--- Scripts/NPCManager.cs
+++ Scripts/NPCManager.cs
@@ -7,25 +7,17 @@ public partial class NPCManager : Node
 
 	CharacterLoader characterLoader;
 	NPCGenerator npcGenerator;
+	RandomNumberGenerator rnd;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        rnd = new RandomNumberGenerator();
 		npcGenerator = new NPCGenerator();
 		characterLoader = new CharacterLoader();
 		// This class should
 		// TODO this is just for testing
-		Character father = npcGenerator.GenerateNPC(new AgeRange(20, 40), Gender.MALE);
-		Character mother = npcGenerator.GenerateNPC(new AgeRange(20, 40), Gender.FEMALE);
-		Character child = npcGenerator.GenerateChild(mother, father, 3, "Asgard");
-		father.children.Add(child.id);
-		mother.children.Add(child.id);
-		npcs.Add(father);
-		npcs.Add(mother);
-		npcs.Add(child);
-		// 1. fetch all existing npcs on load
-		// TODO what do if there are no existing to load
-		// 2. Generate relationship map
+		GenerateFamily("Asgard");
 
 		// TODO saving just for testing
 		SaveNpcs();
@@ -36,5 +28,20 @@ public partial class NPCManager : Node
 			GD.Print("Saving to res://Data/NPCs/"+npc.firstName+"_"+npc.lastName+".json");
 			characterLoader.Save(npc, "res://Data/NPCs/"+npc.firstName+"_"+npc.lastName+".json");
 		}
+	}
+
+	public void GenerateFamily(string location) {
+		Character father = npcGenerator.GenerateNPC(new AgeRange(20, 40), Gender.MALE);
+		Character mother = npcGenerator.GenerateNPC(new AgeRange(20, 40), Gender.FEMALE);
+		int numOfChildren = rnd.RandiRange(0, 4);
+		for (int i = 0; i < numOfChildren; i++) {
+			int age = rnd.RandiRange(0, 16);
+			Character child = npcGenerator.GenerateChild(mother, father, age, location);
+			father.children.Add(child.id);
+			mother.children.Add(child.id);
+			npcs.Add(child);
+		}
+		npcs.Add(father);
+		npcs.Add(mother);
 	}
 }
